a78b43c4428b73c61fd5ec5ddfc54c06e6287e20
Gilmar Rodrigues Campelo <gilmarcampelo@gmail.com>
2025-12-11 15:27:33 -0300

    Implementação Inicial do site XPTO

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function avisar() {
+    alert("Você clicou no botão!");
+}
+
+
+
+document.addEventListener('click',() => { 
+    document.getElementById("meuBotao").addEventListener("click", avisar);
+})
